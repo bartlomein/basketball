665da7f68df0fca96e94f13e30ca4ba22d653dfb
Bartlomiej Tyrpien <bartlomein123@gmail.com>
2019-08-05 20:03:18 -0400

    trying to fix first reducer

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import "./App.css";
 function App(props) {
   return (
     <div className="App">
-      <ServerContainer />
+      <ServerContainer props={props} />
     </div>
   );
 }
 const mapDispatchToProps = dispatch => ({
-  dispatch // ← Add this
+  dispatch
 });
 
 export default connect(mapDispatchToProps)(App);
